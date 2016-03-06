7c2347951d8d019614af64853f01f8915f846628
woshilxd912 <lxd4743@gmail.com>
2016-03-05 17:32:47 -0800

    database

--- src/bian/WebDBInfo.java
+++ src/bian/WebDBInfo.java
@@ -1,7 +1,7 @@
 package bian;
 
 public class WebDBInfo {
-	public static final String MYSQL_USERNAME = "c_cs108_xuandong";
+	public static final String MYSQL_USERNAME = "ccs108xuandong";
 	public static final String MYSQL_PASSWORD = "L6w8RFQNwqPhLfBV";
 	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
 	public static final String MYSQL_DATABASE_NAME = "c_cs108_xuandong";

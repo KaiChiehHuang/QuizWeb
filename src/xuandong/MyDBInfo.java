bf72c7f9e8514ee1968c7a4472029e44f65fa9d1
woshilxd912 <lxd4743@gmail.com>
2016-03-05 17:30:22 -0800

    database information changed

--- src/xuandong/MyDBInfo.java
+++ src/xuandong/MyDBInfo.java
@@ -8,9 +8,9 @@ package xuandong;
  */
 public class MyDBInfo {
 	
-	public static final String MYSQL_USERNAME = "ccs108yuan125";
-	public static final String MYSQL_PASSWORD = "89rjwMmqZ9rt9pCq";
+	public static final String MYSQL_USERNAME = "ccs108xuandong";
+	public static final String MYSQL_PASSWORD = "L6w8RFQNwqPhLfBV";
 	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
-	public static final String MYSQL_DATABASE_NAME = "c_cs108_yuan125";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_xuandong";
 
 }

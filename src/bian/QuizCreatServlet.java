b118c3d8c3cf2229d6645ddc0e745ba121e408f0
边边的电脑 <bianbiandediannao@DN51skga.SUNet>
2016-03-08 13:49:28 -0800

    create quiz jsp

---
+++ src/bian/QuizCreatServlet.java
@@ -0,0 +1,56 @@
+package bian;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import xuandong.Problem;
+import xuandong.Quiz;
+
+/**
+ * Servlet implementation class QuizCreatServlet
+ */
+@WebServlet("/QuizCreatServlet")
+public class QuizCreatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuizCreatServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Quiz quiz = (Quiz)session.getAttribute("newQuiz");
+		
+		ArrayList<Problem> problems = new ArrayList<Problem>();
+		problems = quiz.getProblems();
+		
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("QuizCreatedSuccess.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
